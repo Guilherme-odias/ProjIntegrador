c98955af3e3c70cd26eb07bf14a399a1ce81dcfb
Nicolas <nicolas.seabra09@gmail.com>
2025-09-26 19:50:34 -0300

    Corno

---
+++ Login.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Projeto_integrador
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnLogin = new Button();
+            txtEmail = new TextBox();
+            txtSenha = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(314, 298);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "button1";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(264, 178);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(191, 23);
+            txtEmail.TabIndex = 1;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(264, 236);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(191, 23);
+            txtSenha.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(264, 160);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Email ou User";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(264, 218);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Senha";
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtSenha);
+            Controls.Add(txtEmail);
+            Controls.Add(btnLogin);
+            Name = "Login";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnLogin;
+        private TextBox txtEmail;
+        private TextBox txtSenha;
+        private Label label1;
+        private Label label2;
+    }
+}
