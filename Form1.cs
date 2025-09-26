d1dfd907c9570f4e530f36565b4ddbe9f531aefc
OliLucass <lucas.ali3ns@gmail.com>
2025-09-26 19:47:54 -0300

    cores no forms 1

--- Form1.cs
+++ Form1.cs
@@ -5,7 +5,9 @@ namespace Projeto_integrador
         public Form1()
         {
             InitializeComponent();
-
+            //vermelho botões #a8030c
+            //branco letras #EAEAEA
+            //preto fundo #0A0F1C
         }
     }
 }
--- Sorteador.Designer.cs
+++ Sorteador.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Sorteador";
+            pictureBox1 = new PictureBox();
+            lb_titulo = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.controle_ps1;
+            pictureBox1.Location = new Point(314, 33);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(166, 99);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // lb_titulo
+            // 
+            lb_titulo.AutoSize = true;
+            lb_titulo.Location = new Point(218, 146);
+            lb_titulo.Name = "lb_titulo";
+            lb_titulo.Size = new Size(396, 15);
+            lb_titulo.TabIndex = 1;
+            lb_titulo.Text = "Escolha um Jogo Aleatório da sua Biblioteca Quimera, ou da  Própria Loja ";
+            // 
+            // Sorteador
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lb_titulo);
+            Controls.Add(pictureBox1);
+            Name = "Sorteador";
+            Text = "Sorteador";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label lb_titulo;
     }
 }
