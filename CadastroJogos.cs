82443b1fff865dcf6d396a7dc590abcce8aea6a1
Guilherme-odias <guilherme.odias1@gmail.com>
2025-09-26 19:48:53 -0300

    Conexao

---
+++ CadastroJogos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Projeto_integrador
+{
+    public partial class CadastroJogos : Form
+    {
+        public CadastroJogos()
+        {
+            InitializeComponent();
+            CarregarJogos();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            string nome = txtNomeJogo.Text;
+            string genero = txtGenero.Text;
+            string preco = txtPreco.Text;
+            decimal preco;
+
+            if (string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(genero) || string.IsNullOrWhiteSpace(precoText) || !decimal.TryParse(precoText, out preco))
+            {
+                MessageBox.Show("Por favor, preencha todos os campos corretamente.");
+                return;
+            }
+
+            Jogo jogo = new Jogo
+            {
+                Nome = nome,
+                Genero = genero,
+                Preco = preco
+            };
+
+            jogo.Inserir();
+
+            MessageBox.Show("Jogo cadastrado com sucesso!");
+
+            txtNomeJogo.Clear();
+            txtGenero.Clear();
+            txtPreco.Clear();
+
+            AtualizarGrid();
+
+        }
+
+        private void AtualizarGrid()
+        {
+            dgvJogos.DataSource = null;
+            dgvJogos.DataSource = Jogo.ListarTodos();
+        }
+    }
+}
---
+++ Jogo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Projeto_integrador
+{
+    class Jogo
+    {
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public decimal Preco { get; set; }
+
+        public void Inserir()
+        {
+
+        }
+    }
+}
