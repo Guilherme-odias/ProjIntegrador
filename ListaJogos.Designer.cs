76f14b0615bfd300bb841824c86c84b4f9f5d0be
cauaesdras <cauaesdraslm@gmail.com>
2025-09-29 21:31:33 -0300

    boing

--- ListaJogos.Designer.cs
+++ ListaJogos.Designer.cs
@@ -30,13 +30,18 @@
         {
             label1 = new Label();
             comboBox1 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("SansSerif", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 2);
-            label1.Location = new Point(313, 9);
+            label1.Location = new Point(392, 21);
             label1.Name = "label1";
             label1.Size = new Size(211, 31);
             label1.TabIndex = 0;
@@ -46,20 +51,61 @@
             // 
             comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(139, 168);
+            comboBox1.Items.AddRange(new object[] { "Titulo", "Desenvolvedora", "Distribuidora", "Informacoes" });
+            comboBox1.Location = new Point(83, 113);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 23);
+            comboBox1.Size = new Size(149, 23);
             comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 15.75F);
+            label2.Location = new Point(83, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(149, 24);
+            label2.TabIndex = 2;
+            label2.Text = "Buscar o que:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 15.75F);
+            label3.Location = new Point(270, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 24);
+            label3.TabIndex = 3;
+            label3.Text = "Digite:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(270, 113);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(354, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(83, 185);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(767, 298);
+            dataGridView1.TabIndex = 5;
             // 
             // ListaJogos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(924, 538);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(comboBox1);
             Controls.Add(label1);
             Name = "ListaJogos";
             Text = "ListaJogos";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -68,5 +114,9 @@
 
         private Label label1;
         private ComboBox comboBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
     }
 }
