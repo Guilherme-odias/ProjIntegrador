622eb6ab34bd17fb3289986d3c5a69df9817271a
OliLucass <lucas.ali3ns@gmail.com>
2025-09-30 19:11:06 -0300

    Atualizando

--- Sorteador.Designer.cs
+++ Sorteador.Designer.cs
@@ -33,7 +33,12 @@
             textBox1 = new TextBox();
             btn_bi = new Button();
             btn_loja = new Button();
+            btn_sortear = new Button();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
@@ -53,7 +58,7 @@
             lb_titulo.FlatStyle = FlatStyle.Popup;
             lb_titulo.Font = new Font("SansSerif", 14.2499981F, FontStyle.Italic, GraphicsUnit.Point, 2);
             lb_titulo.ForeColor = Color.FromArgb(234, 234, 234);
-            lb_titulo.Location = new Point(155, 164);
+            lb_titulo.Location = new Point(134, 165);
             lb_titulo.Name = "lb_titulo";
             lb_titulo.Size = new Size(651, 22);
             lb_titulo.TabIndex = 1;
@@ -96,12 +101,63 @@
             btn_loja.Text = "Toda Loja";
             btn_loja.UseVisualStyleBackColor = false;
             // 
+            // btn_sortear
+            // 
+            btn_sortear.BackColor = Color.FromArgb(168, 3, 12);
+            btn_sortear.FlatStyle = FlatStyle.Flat;
+            btn_sortear.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_sortear.ForeColor = Color.FromArgb(234, 234, 234);
+            btn_sortear.Location = new Point(426, 420);
+            btn_sortear.Name = "btn_sortear";
+            btn_sortear.Size = new Size(75, 38);
+            btn_sortear.TabIndex = 5;
+            btn_sortear.Text = "Sortear";
+            btn_sortear.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(369, 490);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(189, 177);
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(168, 3, 12);
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(234, 234, 234);
+            label1.Location = new Point(425, 682);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 19);
+            label1.TabIndex = 7;
+            label1.Text = "Resposta";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(168, 3, 12);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(234, 234, 234);
+            button1.Location = new Point(402, 723);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 49);
+            button1.TabIndex = 8;
+            button1.Text = "Sortear Novamente";
+            button1.UseVisualStyleBackColor = false;
+            // 
             // Sorteador
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(10, 15, 28);
-            ClientSize = new Size(918, 636);
+            ClientSize = new Size(971, 838);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(btn_sortear);
             Controls.Add(btn_loja);
             Controls.Add(btn_bi);
             Controls.Add(textBox1);
@@ -109,7 +165,9 @@
             Controls.Add(pictureBox1);
             Name = "Sorteador";
             Text = "Sorteador";
+            Load += Sorteador_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -121,5 +179,9 @@
         private TextBox textBox1;
         private Button btn_bi;
         private Button btn_loja;
+        private Button btn_sortear;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Button button1;
     }
 }
--- Sorteador.cs
+++ Sorteador.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Projeto_integrador
 {
-    public partial class Sorteador: Form
+    public partial class Sorteador : Form
     {
         public Sorteador()
         {
             InitializeComponent();
+        }
+
+        private void Sorteador_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
