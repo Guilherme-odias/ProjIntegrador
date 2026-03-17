a043efa6822ad3dde4eccdf42d3eec63bed16d17
cauaesdras <cauaesdraslm@gmail.com>
2026-03-16 22:00:18 -0300

    asda
    asdas

---
+++ Site_QuimeraGames/Verificar_email/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suporte ao Cliente</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        
+
+
+        <div class="item">
+
+    <div class="parte_cima">
+        <h2 class="verif_email">Verificar Email</h2>
+        <p class="verif_email_texto">Enviamos um código de verificação no email “xxx@gmail.com”</p>
+    </div>
+
+
+
+            <div class="butaodumeio">
+                <label class="label">Códego</label>
+                <input type="number" name="name" id="codigo" class="codigo"><br>
+
+                <button class="verificar">Verificar</button>
+            
+                <button class="reenviar">Reenviar o código</button>
+            </div>
+            
+
+        </div>
+
+        <footer class="rodape">
+            <button class="btnVoltar" href="">❮</button>
+            <label class="labelVoltar"> Voltar</label>
+        </footer>
+
+    </div>
+
+    
+
+
+
+</body>
+
+</html>
