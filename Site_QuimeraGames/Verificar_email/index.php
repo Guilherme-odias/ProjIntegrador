1c6691830619c4c032f38e223c14551f8bafb938
cauaesdras <cauaesdraslm@gmail.com>
2026-03-18 19:52:28 -0300

    Verificar_email
    front pronto creio eu

--- Site_QuimeraGames/Verificar_email/index.php
+++ Site_QuimeraGames/Verificar_email/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Suporte ao Cliente</title>
-    <link rel="stylesheet" href="styleee.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
