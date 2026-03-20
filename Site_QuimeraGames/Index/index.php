64316722c86c908ea23a4a8e69662767ad7467cd
OliLucass <lucas.ali3ns@gmail.com>
2026-03-19 21:55:50 -0300

    UP

--- Site_QuimeraGames/Index/index.php
+++ Site_QuimeraGames/Index/index.php
@@ -2,7 +2,7 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>QuimeraGames</title>
-  <link rel="stylesheet" href="../Css/stylles.css">
+  <link rel="stylesheet" href="../Css/Stylles.css">
 </head>
 <body>
 
