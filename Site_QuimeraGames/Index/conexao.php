e0854432867217856a7c64566daebb3e73614719
OliLucass <lucas.ali3ns@gmail.com>
2026-03-23 19:21:13 -0300

    Delete conexao.php

--- Site_QuimeraGames/Index/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-function conectar() {
-    try {
-        return new PDO("mysql:host=localhost;dbname=projeto_quimera", "root", "");
-    } catch (PDOException $e) {
-        die("Erro: " . $e->getMessage());
-    }
-}
-?>
