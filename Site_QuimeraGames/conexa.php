6f4d2e48cdcc65eb81411870131804a02e577ebf
OliLucass <lucas.ali3ns@gmail.com>
2026-04-06 20:29:26 -0300

    Update conexa.php

--- Site_QuimeraGames/conexa.php
+++ Site_QuimeraGames/conexa.php
@@ -3,14 +3,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host = "10.37.44.28"; 
+$host = "127.0.0.1"; 
 $db = "projeto_quimera";
 $usuario = "root";
 $senha = "";
 
 try {
 
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $usuario, $senha);
+    $pdo = new PDO("mysql:host=127.0.0.1;port=3306;dbname=$db;charset=utf8", $usuario, $senha);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // echo "Conectado com sucesso!"; // só pra teste
