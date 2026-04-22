23f336cb087cbc5812aff3665389e94b538839d2
Lucas <lucas.ali3ns@gmail.com>
2026-04-22 16:29:43 -0300

    up

--- Site_QuimeraGames/conexa.php
+++ Site_QuimeraGames/conexa.php
@@ -1,22 +1,19 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-$host = "10.37.44.28"; 
+$host = "localhost";
 $db = "projeto_quimera";
 $usuario = "root";
-$senha = "";
+$senha = "admin";
 
 try {
+    $pdo = new PDO(
+        "mysql:host=$host;port=3306;dbname=$db;charset=utf8",
+        $usuario,
+        $senha
+    );
 
-    $pdo = new PDO("mysql:host=$host;port=3306;dbname=projeto_quimera;charset=utf8", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // echo "Conectado com sucesso!"; // só pra teste
-
 } catch (PDOException $e) {
-    die("Erro ao conectar: " . $e->getMessage());
-    
+    die("Erro: " . $e->getMessage());
 }
 ?>
